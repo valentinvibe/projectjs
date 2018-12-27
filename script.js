6d6572ac69bb1ccebe2a9bfa72d312f45adc2e52
Valentin <7705678@gmail.com>
2018-12-27 09:24:19 +0300

    это первый коммит

---
+++ script.js
@@ -0,0 +1,19 @@
+console.log('winline'+'');
+let persone = {
+    name: "Vasya",
+    age: 25,
+    isMArried: true
+}
+console.log('Etot 4yvak - '+persone.name);
+
+let arr = ['fgs','sgsfg','egergerg'];
+console.log(arr[1]);
+/*let answer = confirm('Are you here?');
+console.log(answer);
+let vtor = prompt('Есть ли вам 18',"");
+console.log(vtor);*/
+console.log("2" == 2);
+console.log(5%2);
+let incr = 10,
+    decr = 10;
+console.log(++incr + ++decr);
